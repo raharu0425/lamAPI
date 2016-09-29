d24754c3e4e2a3b180c2dcc294f285d108e6934e
raharu0425 <raharu0425@gmail.com>
2016-09-29 12:51:45 +0900

    refs var_dump消す

--- admin/Console/cake.php
+++ admin/Console/cake.php
@@ -35,7 +35,6 @@ if (function_exists('ini_set')) {
 	} elseif (!file_exists($install . DS . $dispatcher)) {
 		$install = $root . PATH_SEPARATOR .  DS . 'Users' . DS . 'raharu' . DS . 'work' . DS . 'lilycAutoMatonServer' . DS . 'vendors' . DS . 'cakephp' . DS . 'cakephp' . DS . 'lib';
 	}
-var_dump($install);
 
 	ini_set('include_path', $install . PATH_SEPARATOR . ini_get('include_path'));
 	unset($root, $appDir, $install, $composerInstall);
